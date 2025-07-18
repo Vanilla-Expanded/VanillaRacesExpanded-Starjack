54f01cf26c86a10715b474033f19a116aafa141a
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-07-18 09:17:41 +0200

    v0.1

---
+++ 1.6/Source/AbilityComps/CompAbilitySpaceOnly.cs
@@ -0,0 +1,22 @@
+﻿
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace VanillaRacesExpandedStarjack
+{
+    public class CompAbilitySpaceOnly : AbilityComp
+    {
+        public override bool GizmoDisabled(out string reason)
+        {
+            if (this.parent.pawn.Position!=IntVec3.Invalid && this.parent.pawn.Map?.BiomeAt(this.parent.pawn.Position)?.inVacuum != true)
+            {
+                reason = "VRE_AbilityOnlyInSpace".Translate();
+                return true;
+            }
+            return base.GizmoDisabled(out reason);
+        }
+
+       
+    }
+}
